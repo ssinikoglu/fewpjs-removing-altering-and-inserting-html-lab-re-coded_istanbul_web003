85f99dce7f7cc9d74577693c952f11b8209c39b3
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-07-21 01:22:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+main.remove()
+undefined
+let newHeader = document.createElement("h1");
+let attr =  document.createAttribute("id"); 
+attr.value = "victory"; 
+newHeader.setAttributeNode(attr);
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Sule Sinikoglu is the champion"; 
+
+ 
+
+
+
+
+
+
+
